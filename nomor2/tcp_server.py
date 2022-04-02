51c83debd8dc575b071f5c0314014f9ebbffc823
Muhammad Zhafran <frans.zhafran@gmail.com>
2022-04-02 16:22:07 +0700

    feat: nomor 1, 2, dan 3

--- nomor2/tcp_server.py
+++ nomor2/tcp_server.py
@@ -4,6 +4,7 @@ import logging
 import json
 import os
 import ssl
+import threading
 
 alldata = dict()
 alldata['1']=dict(nomor=1, nama="dean henderson", posisi="kiper")
@@ -37,6 +38,52 @@ def proses_request(request_string):
         hasil = None
     return hasil
 
+def accept_connection(koneksi, client_address, is_secure, socket_context):
+    try:
+        if is_secure == True:
+            connection = socket_context.wrap_socket(koneksi, server_side=True)
+        else:
+            connection = koneksi
+
+        selesai=False
+        data_received="" #string
+        while True:
+            data = connection.recv(32)
+            if data:
+                data_received += data.decode()
+                if "\r\n\r\n" in data_received:
+                    selesai=True
+
+                if (selesai==True):
+                    hasil = proses_request(data_received)
+                    logging.warning(f"Sending response: {hasil}")
+
+                    #hasil bisa berupa tipe dictionary
+                    #harus diserialisasi dulu sebelum dikirim via network
+                    # Send data
+                    # some data structure may have complex structure
+                    # how to send such data structure through the network ?
+                    # use serialization
+                    #  example : json, xml
+
+                    # complex structure, nested dict
+                    # all data that will be sent through network has to be encoded into bytes type"
+                    # in this case, the message (type: string) will be encoded to bytes by calling encode
+
+                    hasil = serialisasi(hasil)
+                    hasil += "\r\n\r\n"
+                    connection.sendall(hasil.encode())
+                    selesai = False
+                    data_received = ""  # string
+                    break
+
+            else:
+                logging.warning(f"no more data from {client_address}")
+                break
+        # Clean up the connection
+    except ssl.SSLError as error_ssl:
+        logging.warning(f"SSL error: {str(error_ssl)}")
+
 
 def serialisasi(a):
     #print(a)
@@ -46,6 +93,7 @@ def serialisasi(a):
 
 def run_server(server_address,is_secure=False):
     # ------------------------------ SECURE SOCKET INITIALIZATION ----
+    socket_context = None
     if is_secure == True:
         print(os.getcwd())
         cert_location = os.getcwd() + '/certs/'
@@ -71,52 +119,8 @@ def run_server(server_address,is_secure=False):
         koneksi, client_address = sock.accept()
         print("")
         logging.warning(f"Incoming connection from {client_address}")
-        # Receive the data in small chunks and retransmit it
-
-        try:
-            if is_secure == True:
-                connection = socket_context.wrap_socket(koneksi, server_side=True)
-            else:
-                connection = koneksi
-
-            selesai=False
-            data_received="" #string
-            while True:
-                data = connection.recv(32)
-                if data:
-                    data_received += data.decode()
-                    if "\r\n\r\n" in data_received:
-                        selesai=True
-
-                    if (selesai==True):
-                        hasil = proses_request(data_received)
-                        logging.warning(f"sending response: {hasil}")
-
-                        #hasil bisa berupa tipe dictionary
-                        #harus diserialisasi dulu sebelum dikirim via network
-                        # Send data
-                        # some data structure may have complex structure
-                        # how to send such data structure through the network ?
-                        # use serialization
-                        #  example : json, xml
-
-                        # complex structure, nested dict
-                        # all data that will be sent through network has to be encoded into bytes type"
-                        # in this case, the message (type: string) will be encoded to bytes by calling encode
-
-                        hasil = serialisasi(hasil)
-                        hasil += "\r\n\r\n"
-                        connection.sendall(hasil.encode())
-                        selesai = False
-                        data_received = ""  # string
-                        break
-
-                else:
-                   logging.warning(f"no more data from {client_address}")
-                   break
-            # Clean up the connection
-        except ssl.SSLError as error_ssl:
-            logging.warning(f"SSL error: {str(error_ssl)}")
+        conn_thread = threading.Thread(target=accept_connection, args=(koneksi,client_address, is_secure, socket_context))
+        conn_thread.start()
 
 if __name__=='__main__':
     try:
@@ -125,4 +129,4 @@ if __name__=='__main__':
         logging.warning("Control-C: Program berhenti")
         exit(0)
     finally:
-        logging.warning("seelsai")
+        logging.warning("selesai")
--- nomor3/tcp_client.py
+++ nomor3/tcp_client.py
@@ -19,16 +19,32 @@ def make_socket(destination_address='localhost',port=12000):
     except Exception as ee:
         logging.warning(f"error {str(ee)}")
 
+def make_secure_socket(destination_address='localhost',port=10000):
+    try:
+        #get it from https://curl.se/docs/caextract.html
+
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.verify_mode=ssl.CERT_OPTIONAL
+        context.load_verify_locations(os.getcwd() + '/certs/domain.crt')
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (destination_address, port)
+        sock.connect(server_address)
+        secure_socket = context.wrap_socket(sock,server_hostname=destination_address)
+        return secure_socket
+    except Exception as ee:
+        logging.warning(f"error {str(ee)}")
+
 def deserialisasi(s):
     return json.loads(s)
     
 
-def send_command(command_str,is_secure=False):
+def send_command(command_str,is_secure=True):
     alamat_server = server_address[0]
     port_server = server_address[1]
 #    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # gunakan fungsi diatas
-    sock = make_socket(alamat_server,port_server)
+    sock = make_secure_socket(alamat_server,port_server)
     
     try:
         sock.sendall(command_str.encode())
